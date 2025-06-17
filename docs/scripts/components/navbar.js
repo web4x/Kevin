207a9d65a360d20e16400d0036d0ea115ff2dd7b
Kevin Riedel <1234gold1234gold123456789@gmail.com>
2025-06-17 23:08:43 +0200

    Complete URL

--- docs/scripts/components/navbar.js
+++ docs/scripts/components/navbar.js
@@ -1,15 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navbar = document.getElementById('navbar');
-  if (!navbar) return;
-
-  // Prüfe, ob wir auf der Startseite oder in /pages/ sind
-  const isRoot = location.pathname.endsWith('index.html') || location.pathname === '/' || location.pathname === '/my-web-project/';
-  const prefix = isRoot ? '' : '../';
+  if (navbar) {
+  const base = '/my-web-project/';
 
   navbar.innerHTML = `
-    <a href="${prefix}index.html">Home</a>
-    <a href="${prefix}pages/about.html">Über uns</a>
-    <a href="${prefix}pages/services.html">Leistungen</a>
-    <a href="${prefix}pages/contact.html">Kontakt</a>
-  `;
+    <a href="https://web4x.github.io/Kevin/">Home</a>
+    <a href="https://web4x.github.io/Kevin/about.html">Über uns</a>
+    <a href="https://web4x.github.io/Kevin/services.html">Leistungen</a>
+    <a href="https://web4x.github.io/Kevin//contact.html">Kontakt</a>
+ 
 });
