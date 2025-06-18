129a0fc51d8c60ada74b847d9136fb45114af87d
Kevin Riedel <1234gold1234gold123456789@gmail.com>
2025-06-18 13:36:11 +0200

    Back to four times

--- docs/scripts/components/navbar.js
+++ docs/scripts/components/navbar.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const navbar = document.getElementById('navbar');
   if (navbar) {
-  const base = '/my-web-project/';
-
-  navbar.innerHTML = `
-    <a href="https://web4x.github.io/Kevin/">Home</a>
-    <a href="https://web4x.github.io/Kevin/pages/about.html">Über uns</a>
-    <a href="https://web4x.github.io/Kevin/pages/services.html">Leistungen</a>
-    <a href="https://web4x.github.io/Kevin/pages/contact.html">Kontakt</a>
- 
+    navbar.innerHTML = `
+      <a href="index.html">Home</a>
+      <a href="pages/about.html">Über uns</a>
+      <a href="pages/services.html">Leistungen</a>
+      <a href="pages/contact.html">Kontakt</a>
+    `;
+  }
 });
